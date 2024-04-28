77ba27e2d28f6e8869d6e21c4526f31b3431dde2
abroche <abroche24@yahoo.com>
2024-04-28 19:45:00 -0400

    Project setup

---
+++ src/src/Join.java
@@ -0,0 +1,20 @@
+public class Join {
+
+
+    /*
+    Section 2
+
+    - build a hash table on dataset-A with 50 buckets (initialize to 0 and null)
+    - buckets store the entire record content
+    - hashing of each record to know the bucket it is in should be based on the join column
+        - the sql command gives the which column is the join column
+    - for loop to read dataset-B file-by-file and record-by-record
+        - apply same hash function on join column to know the bucket to check from dataset-A
+    - if A.RandomV = B.RandomV then output the record with the needed columns
+      from the sql command
+
+    - print out the execution time taken to execute the command
+    - print out the records that qualify
+     */
+
+}
