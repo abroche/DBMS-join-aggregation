46ee3222c0cefafe7bf68a5afe699561249b5474
abroche <abroche24@yahoo.com>
2024-04-28 19:59:12 -0400

    User input on SQL command

--- src/src/Join.java
+++ src/src/Join.java
@@ -1,10 +1,17 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Hashtable;
+
 public class Join {
 
+    int capacity = 50;
+    Hashtable<String, String> hashTableA= new Hashtable<>(capacity);
 
     /*
     Section 2
 
-    - build a hash table on dataset-A with 50 buckets (initialize to 0 and null)
+    - build a hash table on dataset-A with 50 buckets
     - buckets store the entire record content
     - hashing of each record to know the bucket it is in should be based on the join column
         - the sql command gives the which column is the join column
@@ -16,5 +23,32 @@ public class Join {
     - print out the execution time taken to execute the command
     - print out the records that qualify
      */
+
+
+
+
+
+
+    /**
+     * Reads the specified file
+     *
+     * @param fileNumber the blockId that is meant to be read
+     * @return the contents of the file
+     */
+    public String readFile(int fileNumber) {
+        String filename = "Project1/F" + fileNumber + ".txt";
+        String blockContent = null;
+        try {
+            FileReader fileReader = new FileReader(filename);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+            blockContent = bufferedReader.readLine();
+
+            bufferedReader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return blockContent;
+    }
 
 }
--- src/src/Main.java
+++ src/src/Main.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the command: ");
+        String input = scanner.nextLine();
+        String cmd = "SELECT A.Col1, A.Col2, B.Col1, B.Col2 FROM A, B WHERE A.RandomV = B.RandomV";
+
+        if (input.equals(cmd)) {
+            System.out.println("User input matches the predefined SQL command.");
+        } else {
+            System.out.println("User input does not match the predefined SQL command.");
+        }
+
+        scanner.close();
+
+
     }
 }
